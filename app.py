289af49124be23aa559a7e1256daade43e819874
Riya Shah <riyaaa.2502shahhh@gmail.com>
2023-01-12 10:16:53 +0530

    update prediction services

--- app.py
+++ app.py
@@ -12,28 +12,6 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__ , static_folder=static_dir , template_folder=template_dir)
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "something went wrong!! Try again"}
-        return render_template("404.html", error=error)
 
 
 @app.route("/" ,methods = ["GET" , "POST"])
